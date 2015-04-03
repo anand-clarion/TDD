ad3ce1d7fc67164ede18836843c84e0242b9eb02
anand-clarion <anand.tripathi@clariontechnologies.co.in>
2015-04-03 13:38:29 +0530

    Added test cases for the user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                       uniqueness: { case_sensitive: false }
 
+  validates :password,  presence: true, confirmation: true
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150402102203) do
+ActiveRecord::Schema.define(version: 20150403071048) do
 
   create_table "microposts", force: :cascade do |t|
     t.string   "content"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150402102203) do
     t.datetime "updated_at"
     t.string   "password"
     t.string   "password_confirmation"
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

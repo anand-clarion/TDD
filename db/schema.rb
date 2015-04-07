093b55731a93b72bd9f9cdda209c841903dc2dcc
anand-clarion <anand.tripathi@clariontechnologies.co.in>
2015-04-07 19:37:50 +0530

    Added sign-in and sign-out functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150403071048) do
+ActiveRecord::Schema.define(version: 20150406135518) do
 
   create_table "microposts", force: :cascade do |t|
     t.string   "content"
@@ -28,8 +28,10 @@ ActiveRecord::Schema.define(version: 20150403071048) do
     t.string   "password"
     t.string   "password_confirmation"
     t.string   "password_digest"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
